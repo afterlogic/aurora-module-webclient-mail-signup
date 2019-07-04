b73c5241bd40704e511613a019f5eab470eab185
nadine-shi <nadine@afterlogic.com>
2019-07-04 13:11:50 +0300

    AuthToken should be set only in App

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -169,7 +169,7 @@ CMainView.prototype.onSignupResponse = function (oResponse, oRequest)
 	}
 	else
 	{
-		$.cookie('AuthToken', oResponse.Result.AuthToken, { expires: 30 });
+		App.setAuthToken(oResponse.Result.AuthToken);
 
 		if (window.location.search !== '' &&
 			UrlUtils.getRequestParam('reset-pass') === null &&
