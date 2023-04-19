b4ae7ac0cc773fc7193bd0a99ed3b37e87d33cb0
Vasil Sokolov <vasil@afterlogic.com>
2023-04-19 13:39:16 +0200

    Made getModuleSettings method public

--- Module.php
+++ Module.php
@@ -33,7 +33,7 @@ class Module extends \Aurora\System\Module\AbstractModule
      *
      * @return Settings
      */
-    protected function GetModuleSettings()
+    public function getModuleSettings()
     {
         return $this->oModuleSettings;
     }
